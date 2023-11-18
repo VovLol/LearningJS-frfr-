69dfce864fb036d3b7648dc8f79d4fd271133831
Kirill <kirillkotloff@gmail.com>
2023-11-18 15:58:43 +0300

    www

--- dum.js
+++ dum.js
@@ -1,1 +1,14 @@
-alert('чоооо')
+let lkg = document.getElementById('kg');
+let funt = document.getElementById('funt');
+let resukt = document.getElementById('resukt');
+
+console.log(funt);
+function convertKgToFUUUUUUUUUUUUUUUUUUUUUUUUUnt(){
+    let kg = lkg.value;
+    let fnt = funt.value;
+    const a = 0.454;
+    let result = kg / a;
+    console.log('результат в фунтах ' + result + ' киллограмма');
+    resukt.innerText = result;
+}
+
