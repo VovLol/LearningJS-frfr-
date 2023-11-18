297bc5dfa1df89f16e1645138be918f3da356443
Kirill <kirillkotloff@gmail.com>
2023-11-18 16:13:36 +0300

    d

--- dum.js
+++ dum.js
@@ -1,14 +1,8 @@
-let lkg = document.getElementById('kg');
-let funt = document.getElementById('funt');
-let resukt = document.getElementById('resukt');
-
-console.log(funt);
-function convertKgToFUUUUUUUUUUUUUUUUUUUUUUUUUnt(){
-    let kg = lkg.value;
-    let fnt = funt.value;
-    const a = 0.454;
-    let result = kg / a;
-    console.log('результат в фунтах ' + result + ' киллограмма');
-    resukt.innerText = result;
-}
-d
+let condition = false;
+let userNmae = prompt('Введите своё имя!');
+console.log(userNmae);
+if(userNmae == "Putin"){
+    alert('YOu are a putin guy')
+}else{
+    alert("Dum bitchass nigga")
+};
