8412a4f4a60851f721fc3b6369ba98f654b8f178
Kirill <kirillkotloff@gmail.com>
2023-12-02 17:55:18 +0300

    top car

--- dum.js
+++ dum.js
@@ -1,102 +1,36 @@
+const buttLeft = document.getElementById('left');
+const buttRight = document.getElementById('right');
+const buttForward = document.getElementById('forward');
+const buttBack = document.getElementById('back');
+let monitor = document.getElementById('mon');
+const folder = "img/tanks/";
+const arrTrans = ["logan", "samosval", "t34", "t90", "uaz"];
+let currTrans = 4;
 
-//  const f = sum;
-//  f(5, 6);
-//  const d = (c, u)=>{
-//     let res = c * u;
-//     console.log(` результат умножения ${res}`);
-//  }
+function runLeft(){
+ console.log("Дядя нахуй пошел игра не доделана закрой сайт чушпан");
+ let path = `${folder}${arrTrans[currTrans]}/left.jpg`
+ monitor.setAttribute('src', path);
+};
+function runForward(){
+    console.log("Дядя нахуй пошел игра не доделана закрой сайт чушпан");
+    let path = `${folder}${arrTrans[currTrans]}/up.jpg`
+    monitor.setAttribute('src', path);
+};
+function runBack(){
+    console.log("Дядя нахуй пошел игра не доделана закрой сайт чушпан");
+    let path = `${folder}${arrTrans[currTrans]}/down.jpg`
+    monitor.setAttribute('src', path);
+};
+function runRight(){
+    let path = `${folder}${arrTrans[currTrans]}/right.jpg`
+    monitor.setAttribute('src', path);
+    console.log("Дядя нахуй пошел игра не доделана закрой сайт чушпан");
 
-//  d(5, 20);
+};
 
+buttLeft.addEventListener('click', runLeft);
+buttForward.addEventListener('click', runForward);
+buttBack.addEventListener('click', runBack);
+buttRight.addEventListener('click', runRight);
 
-// let c = "глобик типиаиаиаиаиаиааиаиаиаиаи";
-
-// function sum(a, b){
-//     c = a +b;
-//     console.log(`бля хуй  ${c}`);
-//     if(c => 12){
-//         let c = "Область дебила";
-//         console.log(c);
-//     }
-//     return c;
-// }
-// sum(5, 7);
-// console.log(c);
-
-// let polkan = {
-//     poroda : 'alabai',
-//     speak: function() {
-//         console.log(`Гав Гав  ${this.age}`)
-//     },
-//     age: 100,
-// }
-
-// // console.table(polkan);
-
-// polkan.speak();
-
-
-let ar = [2,7,4,9,12,33];
-const filt = (a) => a > 5;
-let f = ar.filter(filt);
-console.log(f);
-
-	
-let users =[
-{id:	1,
-name:	"Leanne Graham",
-username:	"Bret",
-age: 25,
-email:	"Sincere@april.biz",
-address: {
-street:	"Kulas Light",
-suite:	"Apt. 556",
-city:	"Gwenborough",
-zipcode:	"92998-3874"}
-},
-{
-   id:	2,
-name:	"Ervin Howell",
-username:	"Antonette",
-age: 25,
-email:	"Shanna@melissa.tv",
-address	:{
-street:	"Victor Plains",
-suite:	"Suite 879",
-city:	"Wisokyburgh",
-zipcode:	"90566-7771"}
-},
-{
-
-id:	3,
-name:	"Clementine Bauch",
-username:	"Samantha",
-age: 12,
-email:	"Nathan@yesenia.net",
-address:	{
-street:	"Douglas Extension",
-suite:	"Suite 847",
-city:	"McKenziehaven",
-zipcode:	"59590-4157"}
-},
-]
-
-1.
-
-let res = users.filter(a => a.age == 25);
-console.table(res);
-
-let res2 = users.map(a => a.email);
-console.table(res2);
-
-let res3 = users.filter(n => {
-    if(n.username.indexOf('a') == -1 && n.username.indexOf('A')== -1){
-        return true;
-    }
-    return false;
-}
- );
-console.table(res3);
-
-let res4 = users.map(a => a.address.street);
-console.table(res4);
