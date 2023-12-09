1b6b622581d4e5c148d5cfa6a6d8d4d2e0831498
Kirill <kirillkotloff@gmail.com>
2023-12-09 18:05:25 +0300

    top

--- dum.js
+++ dum.js
@@ -3,27 +3,80 @@ const buttRight = document.getElementById('right');
 const buttForward = document.getElementById('forward');
 const buttBack = document.getElementById('back');
 let monitor = document.getElementById('mon');
+let changeCar = document.getElementsByTagName('select')[0];
+let imgleft = document.getElementById('lefty');
+let imgright = document.getElementById('righty');
+let imgup = document.getElementById('up');
+let imgdown = document.getElementById('down');
+let groupBtn = document.getElementById('groupBtn');
+groupBtn.addEventListener('click', groupSelection);
+
+let currTrans = 0;
+if(localStorage.getItem('cur')){
+    currTrans = localStorage.getItem('cur');
+}
+
+changeCar.addEventListener('change', function(){
+    currTrans = changeCar.options.selectedIndex - 1;
+    console.log(currTrans);
+    curFolder = `${folder}${arrTrans[currTrans].path}/`;
+    let path = `${curFolder}left.jpg`;
+    monitor.setAttribute('src', path);
+    /////////
+    path = `${curFolder}left.jpg`;
+    imgleft.setAttribute('src', path);
+    path = `${curFolder}right.jpg`;
+    imgright.setAttribute('src', path);
+    path = `${curFolder}up.jpg`;
+    imgup.setAttribute('src', path);
+    path = `${curFolder}down.jpg`;
+    imgdown.setAttribute('src', path);
+
+    localStorage.setItem('cur', currTrans)
+});
+
+function groupSelection(e){
+    console.log(e.target);
+}
+
 const folder = "img/tanks/";
-const arrTrans = ["logan", "samosval", "t34", "t90", "uaz"];
-let currTrans = 4;
+const arrTrans = [{path:"logan", 
+                  carName:"Логан"},
+                  {path:"samosval",
+                   carName:"Сальсан"},
+                   {path:"t34",
+                    carName:"Т34"},
+                    {path:"t90",
+                    carName:"ТЭ90"},
+                    {path:"uaz",
+                     carName:"Уазик"}];
+
+let curFolder = `${folder}${arrTrans[currTrans].path}/`;
+
+for(i =0; i < arrTrans.length ; i++){
+let eloption = document.createElement("option");
+eloption.setAttribute("value", i);
+eloption.textContent = arrTrans[i].carName;
+changeCar.append(eloption);
+}
 
 function runLeft(){
  console.log("Дядя нахуй пошел игра не доделана закрой сайт чушпан");
- let path = `${folder}${arrTrans[currTrans]}/left.jpg`
+let path = `${curFolder}left.jpg`;
  monitor.setAttribute('src', path);
 };
 function runForward(){
     console.log("Дядя нахуй пошел игра не доделана закрой сайт чушпан");
-    let path = `${folder}${arrTrans[currTrans]}/up.jpg`
+    let path = `${curFolder}up.jpg`;
     monitor.setAttribute('src', path);
 };
 function runBack(){
     console.log("Дядя нахуй пошел игра не доделана закрой сайт чушпан");
-    let path = `${folder}${arrTrans[currTrans]}/down.jpg`
+    let path = `${curFolder}down.jpg`;
     monitor.setAttribute('src', path);
 };
 function runRight(){
-    let path = `${folder}${arrTrans[currTrans]}/right.jpg`
+    let path = `${curFolder}right.jpg`;
     monitor.setAttribute('src', path);
     console.log("Дядя нахуй пошел игра не доделана закрой сайт чушпан");
 
