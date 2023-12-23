9ee7c07cb23b9c98e9da0a6fe0596281a9fe2313
Kirill <kirillkotloff@gmail.com>
2023-12-23 17:54:10 +0300

    topi

--- dum.js
+++ dum.js
@@ -1,102 +1,99 @@
+/* let dogs = ['Bobik', 'Ayza', 'Tuskanchik', 'Alabai'];
 
-//  const f = sum;
-//  f(5, 6);
-//  const d = (c, u)=>{
-//     let res = c * u;
-//     console.log(` результат умножения ${res}`);
-//  }
+[bo, ayz] = dogs;
 
-//  d(5, 20);
+console.log(bo, ayz);
+
+[ayz, bo] = [bo, ayz];
+console.log(bo, ayz);
+
+let car = {
+    wheel: "12w",
+    accelerator: "top",
+    color: "red",
+};
+
+let {color: r, wheel} = car;
+console.log(r, wheel);
+ */
+
+let nigger ={
+    slave: "Top",
+    heart: "Free",
+    fecals: "100",
+};
+
+let white_people ={
+    heart: "Free",
+    fecals: "100",
+};
+
+let a = 5; 
+let b;
+b =a;
+console.log(`a = ${a} b= ${b}`);
+
+b = 1;
+console.log(`a = ${a} b= ${b}`);
+
+// nigger = white_people;
+// console.table(nigger);
+// nigger.slave = "free";
+// console.log("people are equal")
+// console.table(nigger);
+// console.log("also cool peopl");
+// console.table(white_people);
 
 
-// let c = "глобик типиаиаиаиаиаиааиаиаиаиаи";
-
-// function sum(a, b){
-//     c = a +b;
-//     console.log(`бля хуй  ${c}`);
-//     if(c => 12){
-//         let c = "Область дебила";
-//         console.log(c);
-//     }
-//     return c;
+// Dog.prototype.speak = function(){
+//     console.log("Gaw");
 // }
-// sum(5, 7);
-// console.log(c);
 
-// let polkan = {
-//     poroda : 'alabai',
-//     speak: function() {
-//         console.log(`Гав Гав  ${this.age}`)
-//     },
-//     age: 100,
-// }
+// class Dog {
+//     constructor(age, Dname, weight, poroda){
+//         this.age = age;
+//         this.Dname = Dname;
+//         this.weight = weight;
+//         this.poroda = poroda;
+//     };
 
-// // console.table(polkan);
+//     speak(){
+//         console.log("GawGaw");
+//     };
+// };
 
-// polkan.speak();
+// let shar = new Dog(2, "Nigger", 5, "Ryzar ayza");
+// shar.age = 1;
 
+// console.table(shar);
+// shar.speak();
 
-let ar = [2,7,4,9,12,33];
-const filt = (a) => a > 5;
-let f = ar.filter(filt);
-console.log(f);
-
-	
-let users =[
-{id:	1,
-name:	"Leanne Graham",
-username:	"Bret",
-age: 25,
-email:	"Sincere@april.biz",
-address: {
-street:	"Kulas Light",
-suite:	"Apt. 556",
-city:	"Gwenborough",
-zipcode:	"92998-3874"}
-},
-{
-   id:	2,
-name:	"Ervin Howell",
-username:	"Antonette",
-age: 25,
-email:	"Shanna@melissa.tv",
-address	:{
-street:	"Victor Plains",
-suite:	"Suite 879",
-city:	"Wisokyburgh",
-zipcode:	"90566-7771"}
-},
-{
-
-id:	3,
-name:	"Clementine Bauch",
-username:	"Samantha",
-age: 12,
-email:	"Nathan@yesenia.net",
-address:	{
-street:	"Douglas Extension",
-suite:	"Suite 847",
-city:	"McKenziehaven",
-zipcode:	"59590-4157"}
-},
-]
-
-1.
-
-let res = users.filter(a => a.age == 25);
-console.table(res);
-
-let res2 = users.map(a => a.email);
-console.table(res2);
-
-let res3 = users.filter(n => {
-    if(n.username.indexOf('a') == -1 && n.username.indexOf('A')== -1){
-        return true;
+class Staff{
+    #userName;
+    #age;
+    constructor(uname, age){
+        this.#userName = uname;
+        this.#age = age;
     }
-    return false;
-}
- );
-console.table(res3);
 
-let res4 = users.map(a => a.address.street);
-console.table(res4);
+    getName(){
+        console.log(this.#userName);
+    };
+};
+
+
+class Manager{
+    valueSale;
+    constructor(uname, age, sale){
+        super(uname, age);
+        this.valueSale = 10;
+
+    };
+    resultSale(){
+        console.log(this.valueSale);
+    }
+};
+
+let semen = new Manager('Semer', 25, 10);
+
+semen.resultSale
